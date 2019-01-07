ce0c899ca225773991f4ade75485952658584ef0
solrSky <295846891@qq.com>
2019-01-07 11:56:33 +0800

    提交：测试类

---
+++ gupaoedu-pattern-template/src/com/carlos/template/test/TemplateTest.java
@@ -0,0 +1,14 @@
+package com.carlos.template.test;
+
+import com.carlos.template.JDBCTemplate;
+
+/**
+ * @author Solrsky
+ * @date 2019/1/7
+ */
+public class TemplateTest {
+
+    public static void main(String[] args) {
+        JDBCTemplate jdbcTemplate = new JDBCTemplate(null);
+    }
+}
